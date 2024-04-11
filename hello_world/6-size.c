bd43bfd7f50a07ec4f2d2acb04440aae5b5d2b18
carlos13suarez <charliesuarez10@gmail.com>
2024-04-11 18:00:11 -0500

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	sizeint = sizeof(int);
+	sizechar = sizeof(char);
+	sizelongint = sizeof(long int);
+	sizelonglongint = sizeof(long long int);
+	sizefloat = sizeof(float)
+	printf("Size of a char: %d byte(s)\n", sizeint);
+	printf("Size of an int: %d byte(s)\n", sizechar);
+	printf("Size of a long int: %d byte(s)\n", sizelongint);
+	printf("Size of a long long int: %d byte(s)\n", sizelonglongint);
+	printf("Size of a float: %d byte(s)", sizefloat);
+	return (0);
+}
