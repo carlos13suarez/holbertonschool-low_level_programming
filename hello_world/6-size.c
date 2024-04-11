5c55e9fbf3013a2e24e78e03d65b81c3b3613930
carlos13suarez <charliesuarez10@gmail.com>
2024-04-11 18:01:43 -0500

    task 6 fix 0: fix declare by adding int in the beginning of declaration

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	sizeint = sizeof(int);
-	sizechar = sizeof(char);
-	sizelongint = sizeof(long int);
-	sizelonglongint = sizeof(long long int);
-	sizefloat = sizeof(float)
+	int sizeint = sizeof(int);
+	int sizechar = sizeof(char);
+	int sizelongint = sizeof(long int);
+	int sizelonglongint = sizeof(long long int);
+	int sizefloat = sizeof(float)
 	printf("Size of a char: %d byte(s)\n", sizeint);
 	printf("Size of an int: %d byte(s)\n", sizechar);
 	printf("Size of a long int: %d byte(s)\n", sizelongint);
