f9cd23c383600d5bf2e3f688eb7b3bfe6163b77e
carlos13suarez <charliesuarez10@gmail.com>
2024-04-11 20:39:40 -0500

    task 9 fix 1: replace * for [] in message declaration var

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t length_message = sizeof(message)/sizeof(message[0]);
 	write(2,message,length_message);
 	return (1);
