b6d21747e42b210ee7a79275dbed13e7842d61b5
carlos13suarez <charliesuarez10@gmail.com>
2024-04-14 01:12:07 -0500

    task 12 fix 0: betty compliance

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		for (j = i + 1; j < 100; j++)
 		{
-			putchar((i/10)+'0');
-			putchar((i%10)+'0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(32);
-			putchar((j/10)+'0');
-			putchar((j%10)+'0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			if (i != 98 || j != 99)
 			{
 				putchar(44);
