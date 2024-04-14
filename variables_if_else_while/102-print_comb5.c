7f21b11ce7c7311367fcd871c427760f1b25ce90
carlos13suarez <charliesuarez10@gmail.com>
2024-04-13 23:09:28 -0500

    task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					if (i != 57 || j != 57 || k != 57 || l != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
