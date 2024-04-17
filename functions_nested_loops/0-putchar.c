9c63328cddfc769e8e9057369a21a9a9fd4887dc
carlos13suarez <charliesuarez10@gmail.com>
2024-04-16 21:14:12 -0500

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -9,6 +9,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
