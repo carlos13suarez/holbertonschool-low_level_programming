5c1942d4dec25d548f3b815e971e6055bfd585eb
carlos13suarez <charliesuarez10@gmail.com>
2024-04-16 23:07:40 -0500

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - function
  *
- * Description: Prints 10 times the alphabet in lowercase, followed by a new 
+ * Description: Prints 10 times the alphabet in lowercase, followed by a new
  * line.
  *
  * Return: Always 0 (Success).
