8a9c6d9fe5e075733a43a0ea35cf7416c0274760
carlos13suarez <charliesuarez10@gmail.com>
2024-04-16 23:00:41 -0500

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - function
  *
  * Description: Prints the alphabet in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success).
  */
-
 void print_alphabet(void)
 {
 	char ch;
