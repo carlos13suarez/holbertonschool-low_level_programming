cd10739ecbb9a3ff3a0c25b1cf9224044b3e6306
carlos13suarez <charliesuarez10@gmail.com>
2024-04-17 22:43:35 -0500

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * Description: Adds two integers.
+ *
+ * Return: Returns the result of the addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
