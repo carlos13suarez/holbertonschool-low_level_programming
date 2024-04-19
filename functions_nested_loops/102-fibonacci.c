01b7e279bbc085c002354a767f3d54698308cf92
carlos13suarez <charliesuarez10@gmail.com>
2024-04-19 16:33:01 -0500

    task 14

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: Prints the first 50 Fibonacci numbers separated by comma,
+ * followed by a space, starting with 1 and 2, followed by a new line.
+ * Return: Always 0. Successful.
+ */
+int main(void)
+{
+	int i;
+	unsigned long int sum, fib_num = 1, fib_num1 = 0;
+
+	printf("0, 1, ");
+	for (i = 0; i < 48; i++)
+	{
+		sum = fib_num + fib_num1;
+		printf("%ld", sum);
+		fib_num1 = fib_num;
+		fib_num = sum;
+		if (i != 47)
+			printf(", ");
+	}
+	printf("\n");
+}
