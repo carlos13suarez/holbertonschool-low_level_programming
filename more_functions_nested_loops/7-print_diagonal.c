5cdd6b06bec33bca19e011316f42f9a452f48d10
carlos13suarez <charliesuarez10@gmail.com>
2024-04-24 19:41:03 -0500

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line
+ * @n: number parameter to define how many slashes and new lines to be printed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
