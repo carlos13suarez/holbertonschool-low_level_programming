100fa14ef873c6a937ef496fb91c3380000b4aca
carlos13suarez <charliesuarez10@gmail.com>
2024-05-04 18:41:35 -0500

    task 7 fix 0: fix half of odd length

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts_half - prints half of a string, followed by a new line.
@@ -15,14 +16,17 @@ void puts_half(char *str)
 	{
 		length++;
 	}
+	printf("Length is: %d\n", length);
 
 	if (length % 2 != 0)
 	{
-		half = (length - 1) / 2;
+		half = (length + 1) / 2;
+		printf("Half is: %d\n", half);
 	}
 	else
 	{
 		half = length / 2;
+		printf("Half is: %d\n", half);
 	}
 
 	for (i = 0; str[half] != '\0'; i++, half++)
