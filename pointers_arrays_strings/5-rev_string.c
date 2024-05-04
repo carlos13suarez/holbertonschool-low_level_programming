9e4bfa7051eb43dd56aae3fd5376a52a93977208
carlos13suarez <charliesuarez10@gmail.com>
2024-05-04 12:00:33 -0500

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @str: array of char to print.
+ *
+ * Return: Nothing.
+ */
+void rev_string(char *str)
+{
+	int i, j, len = 0;
+	char c;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	
+	for (i = 0, j = len - 1; i <= j; i++, j--)
+	{
+		c = str[i];
+		str[i] = str[j];
+		str[j] = c;
+	}
+}
