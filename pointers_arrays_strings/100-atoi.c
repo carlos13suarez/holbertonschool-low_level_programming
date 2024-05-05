0e8818c3f19728989bd3a2d3a68fd02e6246cfd2
carlos13suarez <charliesuarez10@gmail.com>
2024-05-04 23:16:17 -0500

    task 10 fix 0: fix range of the if-break statement

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = num * 10 + (s[i] - 48);
-			if (s[i + 1] <= '0' || s[i + 1] >= '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 
