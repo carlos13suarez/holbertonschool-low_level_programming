e573ac5454330a715a2a151dabb1fe2c99556bec
carlos13suarez <charliesuarez10@gmail.com>
2024-05-04 23:13:33 -0500

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @dest: array of char where the string will be pasted.
+ * @src: string to be copied.
+ *
+ * Return: the pointer to dest.
+ */
+int _atoi(char *s)
+{
+	int i, num = 0, sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - 48);
+			if (s[i + 1] <= '0' || s[i + 1] >= '9')
+				break;
+		}
+
+		if (s[i] == '-')
+			sign *= -1;
+		
+		if (s[i] == '+')
+			sign *= 1;
+	}
+
+	num *= sign;
+
+	return (num);
+}
