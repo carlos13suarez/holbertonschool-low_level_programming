41a14fdb55278ce3a8819a0887fff7162f64782e
carlos13suarez <charliesuarez10@gmail.com>
2024-05-09 18:15:31 -0500

    project 2 task 2 fix 1: add two conditions to first for loop and add for loop null

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > i && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
