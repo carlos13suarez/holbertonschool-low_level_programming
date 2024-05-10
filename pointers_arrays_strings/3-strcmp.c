80a13cb3bce09685b4b492ee552226c4593ad8a3
carlos13suarez <charliesuarez10@gmail.com>
2024-05-09 19:07:40 -0500

    task 3 fix 0: solve everythin in a while loop

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,27 +11,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, len_s1 = 0, len_s2 = 0, res = 0;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len_s1++;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
 
-	for (i = 0; s2[i] != '\0'; i++)
-		len_s2++;
-
-	if (len_s1 != len_s2)
-		return (*s1 - *s2);
-
-	for (i = 0; i < len_s1; i++)
-	{
-		if (s1[i] != s2[i])
-			break;
-		else
-			res++;
-	}
-
-	if (res == len_s1)
-		return 0;
-	else
-		return (*s1 - *s2);
+	return (s1[i] - s2[i]);
 }
