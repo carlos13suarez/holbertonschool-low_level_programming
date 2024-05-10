f0783a018fad268808700f49af8de1d9ea44ae45
carlos13suarez <charliesuarez10@gmail.com>
2024-05-09 20:27:52 -0500

    project 2 task 6 fix 0: betty compliance

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 
 	for (; str[i] != '\0'; i++)
 	{
-		if (str[i] == ',' || str[i] ==';' || str[i] == '.') 
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.')
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 
