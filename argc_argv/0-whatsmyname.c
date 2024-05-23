0cc9fd3b92f3e9d3ea88eb18ea3229472a868386
carlos13suarez <charliesuarez10@gmail.com>
2024-05-23 18:58:38 -0500

    task 0 fix 0: betty compliance and return int

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,8 +2,14 @@
 /**
  * main - prints its name, followed by a new line.
  *
+ * @argc: count of the argument vector.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
