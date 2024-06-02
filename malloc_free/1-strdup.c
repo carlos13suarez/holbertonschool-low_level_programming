bb67b4402e51de2c31648459174c2db5e83077d0
carlos13suarez <charliesuarez10@gmail.com>
2024-06-01 22:06:47 -0500

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: Pointer
+ *
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int longitd = 0;
+
+	while (*s != '\0')
+	{
+		longitd++;
+		s++;
+	}
+
+	return (longitd);
+}
+
+
+
+/**
+ * _strdup - duplicates memory
+ *
+ * @str: string to duplicate.
+ *
+ * Return: NULL if there's no memory. Else pointer.
+ */
+char *_strdup(char *str)
+{
+	char *arr = NULL;
+	int i = 0;
+
+	arr = (char *) malloc(_strlen(str) * sizeof(char));
+
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < _strlen(str); i++)
+		arr[i] = str[i];
+
+	return (arr);
+}
