23ed1129506c293ab7fa722b81bbfb6c8aa0ab9d
carlos13suarez <charliesuarez10@gmail.com>
2024-06-06 20:15:02 -0500

    task 3

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+/* Libraries */
+/* Macros */
+#define ABS(x) ((x) < (0) ? (x) * -1 : (x))
+
+/* Structures */
+/* Prototypes */
+
+#endif
