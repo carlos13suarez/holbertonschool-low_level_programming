a9455cdd9387d759697f57b9726d78627519d332
carlos13suarez <charliesuarez10@gmail.com>
2024-06-06 20:40:11 -0500

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+/* Libraries */
+/* Macros */
+/* Structures */
+/**
+ * struct dog - elements of a dog object
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* Prototypes */
+
+#endif
