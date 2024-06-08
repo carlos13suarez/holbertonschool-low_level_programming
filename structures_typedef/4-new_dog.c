1cc826f6d74293b98934054644e79604da20fd59
carlos13suarez <charliesuarez10@gmail.com>
2024-06-08 14:14:00 -0500

    task 4 fix 0: use _strlen

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,26 @@
 #include "dog.h"
 
 /**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: Pointer
+ *
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+        int longitd = 0;
+
+        while (*s != '\0')
+        {
+                longitd++;
+                s++;
+        }
+
+        return (longitd);
+}
+
+/**
  * new_dog - creates a new dog.
  *
  * @name: the name of the dog.
@@ -23,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	dogname = (char *) malloc(strlen(name));
 	dogage = (float *) malloc(sizeof(float));
-	dogowner = (char *) malloc(strlen(owner));
+	dogowner = (char *) malloc(_strlen(owner));
 	if (!dogname | !dogage | !dogowner)
 	{
 		free(dogowner);
