aa67aefcee6bf52ee5b89ed62cac9fd224dde307
carlos13suarez <charliesuarez10@gmail.com>
2024-06-08 14:56:20 -0500

    task 4 fix 2: use _strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,33 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copies string from one char array to another.
+ *
+ * @dest: array of char where the string will be pasted.
+ * @src: string to be copied.
+ *
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	for (i = 0; i <= length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
+
+/**
  * _strlen - function that returns the length of a string
  *
  * @s: Pointer
@@ -19,6 +46,7 @@ int _strlen(char *s)
 
 	return (len);
 }
+
 
 /**
  * new_dog - creates a new dog.
