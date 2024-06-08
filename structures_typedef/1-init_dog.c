326740255b593097e5e3f6875a8ed64f09593c58
carlos13suarez <charliesuarez10@gmail.com>
2024-06-08 11:07:50 -0500

    task 1 fix 0: NULL struct passed case)

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d: struct dog pointer
+ * @name: name element of type char* in the struct dog
+ * @age: age element of type float in the struct dog
+ * @owner: owner element of type char* in the struct dog
+ *
+ * Return: Nothing.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+	{
+		printf("Ok\n");
+		exit(1);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
