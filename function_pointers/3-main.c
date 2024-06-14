ae593117b96376ddb93435058700e58d90443fe0
carlos13suarez <charliesuarez10@gmail.com>
2024-06-13 21:30:46 -0500

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: array of argument vectors
+ *
+ * Return: 0 if succesful, else if failure.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((atoi(argv[2]) == '/' || atoi(argv[2]) == '%') && (atoi(argv[3]) == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = operator(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add operation
+ *
+ * @a: first number to operate
+ * @b: second number to operate
+ *
+ * Return: result of the operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract operation
+ *
+ * @a: first number to operate
+ * @b: second number to operate
+ *
+ * Return: result of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operation
+ *
+ * @a: first number to operate
+ * @b: second number to operate
+ *
+ * Return: result of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divition operation
+ *
+ * @a: first number to operate
+ * @b: second number to operate
+ *
+ * Return: result of the operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo operation
+ *
+ * @a: first number to operate
+ * @b: second number to operate
+ *
+ * Return: result of the operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
