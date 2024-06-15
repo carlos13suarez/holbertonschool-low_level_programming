6978df14c8d4c045edf701cf57a743ebbc437b4d
carlos13suarez <charliesuarez10@gmail.com>
2024-06-15 13:15:54 -0500

    task 0 betty compliance

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  *
- * @n: 
+ * @n: quantity of numbers to add
  *
  * Return: 0 if n == 0. Else, the sum of all its parameters.
  */
@@ -15,12 +15,12 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 
-	va_end (ap);
-	return sum;
+	va_end(ap);
+	return (sum);
 }
