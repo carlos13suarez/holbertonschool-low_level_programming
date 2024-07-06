3b5c41f43bb360ef3fb95476ee76174321bbfb86
carlos13suarez <charliesuarez10@gmail.com>
2024-07-06 11:24:56 -0500

    task 2 fix 0: use _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * _strlen - Function that returns the length of a string.
+ *
+ * @s: Pointer of type char that points to a string.
+ *
+ * Description: Function that returns the number of bytes of a string pointed
+ * by pointer *s.
+ *
+ * Return: Number of bytes of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list.
  *
  * @head: pointer to a pointer to a node
@@ -16,7 +37,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 
 	head = &new_node;
