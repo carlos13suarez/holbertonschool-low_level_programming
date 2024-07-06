29e8ab14bf9d4a999a3f8df00e60365bdf67159d
carlos13suarez <charliesuarez10@gmail.com>
2024-07-06 13:04:06 -0500

    task 5

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * startupFunction - function executed before the main function
+ *
+ * __attribute__ ((constructor)): constructor attribute that allows the
+ * function to be executed before main function gets executed.
+ * 
+ * Return: nothing.
+ */
+void __attribute__ ((constructor)) startupFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
