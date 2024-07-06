3f77253f352ea580c7ddcbd2de0f7a76e4ee6e23
carlos13suarez <charliesuarez10@gmail.com>
2024-07-06 11:26:10 -0500

    task 2 fix 1: use _strlen with strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -37,7 +37,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	new_node->len = _strlen(strdup(str));
 	new_node->next = *head;
 
 	head = &new_node;
