e34f0b31c9fae4c6496c5c288dbf875744703361
carlos13suarez <charliesuarez10@gmail.com>
2024-07-21 12:43:05 -0500

    task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ *
+ * @n: decimal number.
+ * @index: index at the binary representation of n.
+ *
+ * Return: the value 1 or 0 at the given index. -1 if an error occurred.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int k;
+	
+	if (index > (sizeof(n) * 8) - 1)
+		return (-1);
+
+	k = n >> index;
+
+	if (k & 1)
+		return (1);
+	else
+		return (0);
+}
