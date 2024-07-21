9e157ff390dbc4298bf90418d77e420296d73317
carlos13suarez <charliesuarez10@gmail.com>
2024-07-21 11:42:39 -0500

    task 1 fix 1: reverse fix 0 and set c to the size of n

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,19 +9,18 @@
  * The flag will go up once it finds the first 1 and
  * will stay 1 from there. The flag is used for when n == 0,
  * for when it never raises, it will just print a 0.
- * On the other hand, the for loop starts from the magic
- * number 31 (arbitrarily defined from the number of bits
- * an unsigned long int has), so that the code starts
- * looking for the first rightmost 1 bit, and starts
- * printing from right to left.
+ * On the other hand, the for loop starts from the leftmost
+ * bit of the n parameter, which is always 32, so that the
+ * code starts looking for the first rightmost 1 bit, and
+ * starts printing from right to left.
  *
  * Return: Nothing.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int c, k, flag = 0;
+	int c, k, flag = 0;
 
-	for (c = 31; c >= 0; c--)
+	for (c = (sizeof(n) * 8) - 1; c >= 0; c--)
 	{
 		k = n >> c;
 
