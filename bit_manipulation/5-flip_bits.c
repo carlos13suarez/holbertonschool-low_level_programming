ea03a028e8d8ed56a4c246fb8846abd43e5e7003
carlos13suarez <charliesuarez10@gmail.com>
2024-07-22 20:13:49 -0500

    task 5 betty compliance

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,12 +8,12 @@
  * @m: the number that n will become into.
  *
  * Return: returns the number of bits you would need to flip to get from one
- * number to another. 
+ * number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int k = 0, l = 0, count = 0;
-	
+
 	while (!(n == 0 && m == 0))
 	{
 		k = n & 1;
@@ -21,7 +21,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		n = n >> 1;
 		m = m >> 1;
 		if (k ^ l)
-			count ++;
+			count++;
 	}
 
 	return (count);
