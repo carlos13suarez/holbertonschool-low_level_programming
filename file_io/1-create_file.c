725d8ee0249253532f9ba296d5fc25380a82ac46
carlos13suarez <charliesuarez10@gmail.com>
2024-07-25 21:09:51 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,6 +29,7 @@ size_t _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
+	size_t num_letters = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -45,7 +46,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, _strlen(text_content));
+	num_letters = write(fd, text_content, _strlen(text_content));
+	if (num_letters == -1)
+		return (-1);
 
 	if (close(fd) == -1)
 		return (-1);
