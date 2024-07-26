5dd855fe1240e1c3921e6d0547980996dd19ebb7
carlos13suarez <charliesuarez10@gmail.com>
2024-07-25 21:06:00 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of str
  *
+ * @str: string.
+ *
+ * Return: length
  */
 size_t _strlen(char *str)
 {
 	int len = 0;
+
 	while (str[len])
 		len++;
 	return (len);
@@ -18,7 +23,8 @@ size_t _strlen(char *str)
  * @filename: name of the file to create
  * @text_content: a NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
+ * Return: 1 on success, -1 on failure (file can not be created, file can not
+ * be written, write “fails”, etc…)
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -35,7 +41,9 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
 
 	write(fd, text_content, _strlen(text_content));
 
