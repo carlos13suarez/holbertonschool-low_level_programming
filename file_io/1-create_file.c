371e64c7281783c74ccddc3a8c26365a8ad4fe85
carlos13suarez <charliesuarez10@gmail.com>
2024-07-25 20:48:19 -0500

    task 1 fix 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,19 +27,22 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	if (fd == -1)
+	if (text_content == NULL)
 	{
-		perror("open\n");
-		return (-1);
+		fd = open(filename, O_CREATE, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
 	}
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
 	write(fd, text_content, _strlen(text_content));
+
 	if (close(fd) == -1)
-	{
-		perror("close\n");
-		return (0);
-	}
+		return (-1);
 
 	return (1);
 }
