d2f7e2c2404550fed4820772ab78d7c475105906
carlos13suarez <charliesuarez10@gmail.com>
2024-07-27 12:38:30 -0500

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of str
  *
+ * @str: string.
+ *
+ * Return: length
  */
 size_t _strlen(char *str)
 {
 	int len = 0;
+
 	while (str[len])
 		len++;
 	return (len);
@@ -13,41 +18,35 @@ size_t _strlen(char *str)
 
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text at the end of a file.
  *
  * @filename: name of the file to create
- * @text_content: a NULL terminated string to write to the file
+ * @text_content: a NULL terminated string to write to to add at the end of
+ * the file
  *
- * Return: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = 0;
+	int fd = 0, num_letters = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT, 0600);
-		if (fd == -1)
-			return (-1);
-		return (1);
-	}
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND, mode);
 	if (fd == -1)
+		return (-1);
+
+	num_letters = write(fd, text_content, _strlen(text_content));
+	if (num_letters == -1)
 	{
-		perror("open\n");
+		close(fd);
 		return (-1);
 	}
 
-	write(fd, text_content, _strlen(text_content));
 	if (close(fd) == -1)
-	{
-		perror("close\n");
 		return (0);
-	}
 
 	return (1);
 }
