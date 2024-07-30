278ee5faaf9d661f7b9f4845fb470ff108376dc8
carlos13suarez <charliesuarez10@gmail.com>
2024-07-29 20:55:01 -0500

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ *
+ */
+size_t _strlen(char *str)
+{
+	int len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+
+/**
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, bytes_read, bytes_written;
+	char buffer[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1 || argv[1] == NULL)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(fd1);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd1, buffer, 1024)) > 0)
+	{
+		bytes_written = write(fd2, buffer, _strlen(buffer));
+		if (bytes_written == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd1) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+
+	return (0);
+}
