77174762fd45ed6881e91030c3cb64a879a0def7
carlos13suarez <charliesuarez10@gmail.com>
2024-08-05 20:33:26 -0500

    task 0 fix 1: betty compliance

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - creates a hash table
  *
  * @size: size of the array (hash table)
- * 
+ *
  * Return: a pointer to the newly created hash table.
  * Returns NULL if something went wrong
  */
@@ -15,14 +15,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	HashTable = malloc(sizeof(hash_table_t));
 
 	if (!HashTable)
-		return NULL;
+		return (NULL);
 
 	HashTable->size = size;
 	HashTable->array = (hash_node_t **) malloc(size * sizeof(unsigned long int));
 	if (!HashTable->array)
 	{
 		free(HashTable);
-		return NULL;
+		return (NULL);
 	}
 
 	return (HashTable);
