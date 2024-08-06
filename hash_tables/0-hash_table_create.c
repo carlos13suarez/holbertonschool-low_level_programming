aa3b489df90e4720fea3821b444620d650a736d1
carlos13suarez <charliesuarez10@gmail.com>
2024-08-05 20:31:50 -0500

    task 0 fix 0: handle malloc fail cases

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,8 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *HashTable = NULL;
 
 	HashTable = malloc(sizeof(hash_table_t));
+
+	if (!HashTable)
+		return NULL;
+
 	HashTable->size = size;
 	HashTable->array = (hash_node_t **) malloc(size * sizeof(unsigned long int));
+	if (!HashTable->array)
+	{
+		free(HashTable);
+		return NULL;
+	}
 
 	return (HashTable);
 }
