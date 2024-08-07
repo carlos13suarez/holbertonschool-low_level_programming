12f1bd1cf1a5c88a36a10b88b2b2272f481fd8e9
carlos13suarez <charliesuarez10@gmail.com>
2024-08-06 19:54:13 -0500

    task 3 fix 0: replace value instead of key if there's the same key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(currentNode->key, key))
 		{
-			currentNode->key = strdup(key);
+			currentNode->value = strdup(value);
 			return (1);
 		}
 		currentNode = currentNode->next;
