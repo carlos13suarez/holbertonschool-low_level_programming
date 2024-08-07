84c73b5262caa942d4adc28958f5b15828485614
carlos13suarez <charliesuarez10@gmail.com>
2024-08-06 19:44:13 -0500

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *newNode, currentNode;
+	hash_node_t *newNode, *currentNode;
 
 	if (!key || !ht)
 		return (0);
